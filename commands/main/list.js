27cda0424375707694d37db822fb5bd5220d9fa5
smilevideo <andrewk1216@gmail.com>
2023-05-11 05:47:57 -0400

    feat: first commit

---
+++ commands/main/list.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('list')
+		.setDescription('lists all users in the voice channel this message was sent from'),
+	async execute(interaction) {
+		if (!interaction.member.voice) {
+            await interaction.reply('User is not in a voice channel.');
+        } else {
+            const channelMembers = interaction.member.voice.channel.members;
+            let replyString = "Currently in the voice channel:";
+            channelMembers.forEach((member) => {
+                replyString += `\n${member.user.username}, User ID: ${member.user.id}`;
+            });
+
+            await interaction.reply(replyString);
+        };
+	},
+};
